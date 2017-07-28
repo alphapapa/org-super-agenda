79900da39d459c5ed1d66ae56003cc44ed5a8e0d
Adam Porter <adam@alphapapa.net>
2017-07-28 16:54:56 -0500

    Set custom option types

--- org-super-agenda.el
+++ org-super-agenda.el
@@ -110,12 +110,14 @@ Populated automatically by `org-super-agenda--defgroup'.")
   :link '(url-link "http://github.com/alphapapa/org-super-agenda"))
 
 (defcustom org-super-agenda-unmatched-order 99
-  "Default order setting for agenda section containing items unmatched by any filter.")
+  "Default order setting for agenda section containing items unmatched by any filter."
+  :type 'integer)
 
 (defcustom org-super-agenda-fontify-whole-header-line nil
   "Fontify the whole line for section headers.
 This is mostly useful if section headers have a highlight color,
-making it stretch across the screen.")
+making it stretch across the screen."
+  :type 'boolean)
 
 ;;;; Filters
 
