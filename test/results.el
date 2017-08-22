417d2078b229098c4d56a494235ff56cd98a5d8c
Adam Porter <adam@alphapapa.net>
2017-08-22 00:04:44 -0500

    Update tests and results

--- test/results.el
+++ test/results.el
@@ -1,9 +1,9 @@
 #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8 data (311663808 "Day-agenda (W27):
 Wednesday   5 July 2017
   test:        2:02......              Sunrise (12:04 of daylight)
+              12:00......              now - - - - - - - - - - - - - - - - - - - - - - - - -
   test:       14:07......              Sunset 
   test:       18:00...... Scheduled:   dinner      TODO Order a pizza :food:dinner:
-              21:50......              now - - - - - - - - - - - - - - - - - - - - - - - - -
   test:        personal    TODO Practice leaping tall buildings in a                      !       on::personal:
   test:       Sched. 2x:   meetings    TODO [#A] Skype with president of Antarctica :universe:ambition::world:meetings:
   test:       Scheduled:   @town       TODO Shop for groceries :food:shopping:@town:
@@ -108,4 +108,188 @@ Wednesday   5 July 2017
   test:       Scheduled:   programming SOMEDAY Rewrite Emacs in Common Lisp :Emacs:elisp:computers:software:programming:
   test:       Scheduled:   @town       TODO [#C] Get haircut    :personal:@town:
   test:       In  16 d.:   bills       TODO [#B] Internet                :bills:
+" 1425382039519238350 "Day-agenda (W27):
+Wednesday   5 July 2017
+
+ Today
+  test:        2:02......              Sunrise (12:04 of daylight)
+              12:00......              now - - - - - - - - - - - - - - - - - - - - - - - - -
+  test:       14:07......              Sunset 
+  test:       18:00...... Scheduled:   dinner      TODO Order a pizza :food:dinner:
+
+ Other items
+  test:       Sched. 2x:   meetings    TODO [#A] Skype with president of Antarctica :universe:ambition::world:meetings:
+  test:       Scheduled:   @computer   TODO [#B] Fix flux capacitor :spaceship:shopping:@computer:
+  test:       Scheduled:   @town       TODO [#C] Get haircut    :personal:@town:
+  test:       In   2 d.:   world       TODO [#A] Take over the world :universe:ambition::world:
+  test:       In   5 d.:   ambition    TODO [#B] Renew membership in supervillain club :universe:ambition::
+  test:       In  10 d.:   ambition    TODO [#A] Take over the universe :universe:ambition:
+  test:       In  16 d.:   bills       TODO [#B] Internet                :bills:
+  test:       In  27 d.:   spaceship   TODO [#A] Spaceship lease :bills:spaceship:
+
+ Unprioritized
+  test:        personal    TODO Practice leaping tall buildings in a                      !       on::personal:
+  test:       Scheduled:   @town       TODO Shop for groceries :food:shopping:@town:
+  test:       Scheduled:   programming SOMEDAY Rewrite Emacs in Common Lisp :Emacs:elisp:computers:software:programming:
+  test:       Deadline:    Emacs       CHECK /r/emacs            :website:Emacs:
+  test:       In  53 d.:   travel      WAITING Visit the moon :universe:ambition::space:travel:
+  test:       In  77 d.:   planet      TODO Visit Mars :universe:ambition::space:travel:planet:
+" 799044487 "Day-agenda (W27):
+Wednesday   5 July 2017
+
+ Priority >= B items
+  test:       Sched. 2x:   meetings    TODO [#A] Skype with president of Antarctica :universe:ambition::world:meetings:
+  test:       Scheduled:   @computer   TODO [#B] Fix flux capacitor :spaceship:shopping:@computer:
+  test:       In   2 d.:   world       TODO [#A] Take over the world :universe:ambition::world:
+  test:       In   5 d.:   ambition    TODO [#B] Renew membership in supervillain club :universe:ambition::
+  test:       In  10 d.:   ambition    TODO [#A] Take over the universe :universe:ambition:
+  test:       In  16 d.:   bills       TODO [#B] Internet                :bills:
+  test:       In  27 d.:   spaceship   TODO [#A] Spaceship lease :bills:spaceship:
+
+ Other items
+  test:        2:02......              Sunrise (12:04 of daylight)
+              12:00......              now - - - - - - - - - - - - - - - - - - - - - - - - -
+  test:       14:07......              Sunset 
+  test:       18:00...... Scheduled:   dinner      TODO Order a pizza :food:dinner:
+  test:        personal    TODO Practice leaping tall buildings in a                      !       on::personal:
+  test:       Scheduled:   @town       TODO Shop for groceries :food:shopping:@town:
+  test:       Scheduled:   programming SOMEDAY Rewrite Emacs in Common Lisp :Emacs:elisp:computers:software:programming:
+  test:       Deadline:    Emacs       CHECK /r/emacs            :website:Emacs:
+  test:       Scheduled:   @town       TODO [#C] Get haircut    :personal:@town:
+  test:       In  53 d.:   travel      WAITING Visit the moon :universe:ambition::space:travel:
+  test:       In  77 d.:   planet      TODO Visit Mars :universe:ambition::space:travel:planet:
+" 54821847708 "Day-agenda (W27):
+Wednesday   5 July 2017
+
+ Other items
+  test:        2:02......              Sunrise (12:04 of daylight)
+              12:00......              now - - - - - - - - - - - - - - - - - - - - - - - - -
+  test:       14:07......              Sunset 
+  test:       18:00...... Scheduled:   dinner      TODO Order a pizza :food:dinner:
+  test:        personal    TODO Practice leaping tall buildings in a                      !       on::personal:
+  test:       Sched. 2x:   meetings    TODO [#A] Skype with president of Antarctica :universe:ambition::world:meetings:
+  test:       Scheduled:   @town       TODO Shop for groceries :food:shopping:@town:
+  test:       Scheduled:   @computer   TODO [#B] Fix flux capacitor :spaceship:shopping:@computer:
+  test:       Scheduled:   programming SOMEDAY Rewrite Emacs in Common Lisp :Emacs:elisp:computers:software:programming:
+  test:       Deadline:    Emacs       CHECK /r/emacs            :website:Emacs:
+  test:       In   2 d.:   world       TODO [#A] Take over the world :universe:ambition::world:
+  test:       In   5 d.:   ambition    TODO [#B] Renew membership in supervillain club :universe:ambition::
+  test:       In  10 d.:   ambition    TODO [#A] Take over the universe :universe:ambition:
+  test:       In  16 d.:   bills       TODO [#B] Internet                :bills:
+  test:       In  27 d.:   spaceship   TODO [#A] Spaceship lease :bills:spaceship:
+  test:       In  53 d.:   travel      WAITING Visit the moon :universe:ambition::space:travel:
+  test:       In  77 d.:   planet      TODO Visit Mars :universe:ambition::space:travel:planet:
+
+ Priority < B items
+  test:       Scheduled:   @town       TODO [#C] Get haircut    :personal:@town:
+" 801052988 "Day-agenda (W27):
+Wednesday   5 July 2017
+
+ Headings matching regexps: \"moon\"
+  test:       In  53 d.:   travel      WAITING Visit the moon :universe:ambition::space:travel:
+
+ Other items
+  test:        2:02......              Sunrise (12:04 of daylight)
+              12:00......              now - - - - - - - - - - - - - - - - - - - - - - - - -
+  test:       14:07......              Sunset 
+  test:       18:00...... Scheduled:   dinner      TODO Order a pizza :food:dinner:
+  test:        personal    TODO Practice leaping tall buildings in a                      !       on::personal:
+  test:       Sched. 2x:   meetings    TODO [#A] Skype with president of Antarctica :universe:ambition::world:meetings:
+  test:       Scheduled:   @town       TODO Shop for groceries :food:shopping:@town:
+  test:       Scheduled:   @computer   TODO [#B] Fix flux capacitor :spaceship:shopping:@computer:
+  test:       Scheduled:   programming SOMEDAY Rewrite Emacs in Common Lisp :Emacs:elisp:computers:software:programming:
+  test:       Deadline:    Emacs       CHECK /r/emacs            :website:Emacs:
+  test:       Scheduled:   @town       TODO [#C] Get haircut    :personal:@town:
+  test:       In   2 d.:   world       TODO [#A] Take over the world :universe:ambition::world:
+  test:       In   5 d.:   ambition    TODO [#B] Renew membership in supervillain club :universe:ambition::
+  test:       In  10 d.:   ambition    TODO [#A] Take over the universe :universe:ambition:
+  test:       In  16 d.:   bills       TODO [#B] Internet                :bills:
+  test:       In  27 d.:   spaceship   TODO [#A] Spaceship lease :bills:spaceship:
+  test:       In  77 d.:   planet      TODO Visit Mars :universe:ambition::space:travel:planet:
+" 1437579087099685104 "Day-agenda (W27):
+Wednesday   5 July 2017
+
+ Today
+  test:        2:02......              Sunrise (12:04 of daylight)
+              12:00......              now - - - - - - - - - - - - - - - - - - - - - - - - -
+  test:       14:07......              Sunset 
+  test:       18:00...... Scheduled:   dinner      TODO Order a pizza :food:dinner:
+
+ Important
+  test:       In  16 d.:   bills       TODO [#B] Internet                :bills:
+  test:       In  27 d.:   spaceship   TODO [#A] Spaceship lease :bills:spaceship:
+  test:       Sched. 2x:   meetings    TODO [#A] Skype with president of Antarctica :universe:ambition::world:meetings:
+  test:       In   2 d.:   world       TODO [#A] Take over the world :universe:ambition::world:
+  test:       In  10 d.:   ambition    TODO [#A] Take over the universe :universe:ambition:
+
+ Priority <= B items
+  test:       In   5 d.:   ambition    TODO [#B] Renew membership in supervillain club :universe:ambition::
+
+ Personal
+  test:        personal    TODO Practice leaping tall buildings in a                      !       on::personal:
+  test:       Scheduled:   @town       TODO [#C] Get haircut    :personal:@town:
+
+ Shopping in town
+  test:       Scheduled:   @town       TODO Shop for groceries :food:shopping:@town:
+
+ Space-related (non-moon-or-planet-related)
+  test:       Scheduled:   @computer   TODO [#B] Fix flux capacitor :spaceship:shopping:@computer:
+
+ WAITING items
+  test:       In  53 d.:   travel      WAITING Visit the moon :universe:ambition::space:travel:
+
+ SOMEDAY and TO-READ and CHECK and TO-WATCH and WATCHING items
+  test:       Scheduled:   programming SOMEDAY Rewrite Emacs in Common Lisp :Emacs:elisp:computers:software:programming:
+  test:       Deadline:    Emacs       CHECK /r/emacs            :website:Emacs:
+
+ Other items
+  test:       In  77 d.:   planet      TODO Visit Mars :universe:ambition::space:travel:planet:
+" 799029616 "Day-agenda (W27):
+Wednesday   5 July 2017
+
+ Group: bills
+  test:       In  27 d.:   spaceship   TODO [#A] Spaceship lease :bills:spaceship:
+
+ Group: plans
+  test:       In  77 d.:   planet      TODO Visit Mars :universe:ambition::space:travel:planet:
+  test:       In  53 d.:   travel      WAITING Visit the moon :universe:ambition::space:travel:
+  test:       In  10 d.:   ambition    TODO [#A] Take over the universe :universe:ambition:
+  test:       In   5 d.:   ambition    TODO [#B] Renew membership in supervillain club :universe:ambition::
+  test:       In   2 d.:   world       TODO [#A] Take over the world :universe:ambition::world:
+  test:       Sched. 2x:   meetings    TODO [#A] Skype with president of Antarctica :universe:ambition::world:meetings:
+  test:        personal    TODO Practice leaping tall buildings in a                      !       on::personal:
+
+ Group: recurring
+  test:       Deadline:    Emacs       CHECK /r/emacs            :website:Emacs:
+  test:       Scheduled:   @town       TODO Shop for groceries :food:shopping:@town:
+  test:       14:07......              Sunset 
+  test:        2:02......              Sunrise (12:04 of daylight)
+
+ Other items
+              12:00......              now - - - - - - - - - - - - - - - - - - - - - - - - -
+  test:       18:00...... Scheduled:   dinner      TODO Order a pizza :food:dinner:
+  test:       Scheduled:   @computer   TODO [#B] Fix flux capacitor :spaceship:shopping:@computer:
+  test:       Scheduled:   programming SOMEDAY Rewrite Emacs in Common Lisp :Emacs:elisp:computers:software:programming:
+  test:       Scheduled:   @town       TODO [#C] Get haircut    :personal:@town:
+  test:       In  16 d.:   bills       TODO [#B] Internet                :bills:
+" 761420928 "Day-agenda (W27):
+Wednesday   5 July 2017
+
+ Other items
+  test:        2:02......              Sunrise (12:04 of daylight)
+              12:00......              now - - - - - - - - - - - - - - - - - - - - - - - - -
+  test:       14:07......              Sunset 
+  test:        personal    TODO Practice leaping tall buildings in a                      !       on::personal:
+  test:       Sched. 2x:   meetings    TODO [#A] Skype with president of Antarctica :universe:ambition::world:meetings:
+  test:       Scheduled:   @computer   TODO [#B] Fix flux capacitor :spaceship:shopping:@computer:
+  test:       Scheduled:   programming SOMEDAY Rewrite Emacs in Common Lisp :Emacs:elisp:computers:software:programming:
+  test:       Deadline:    Emacs       CHECK /r/emacs            :website:Emacs:
+  test:       Scheduled:   @town       TODO [#C] Get haircut    :personal:@town:
+  test:       In   2 d.:   world       TODO [#A] Take over the world :universe:ambition::world:
+  test:       In   5 d.:   ambition    TODO [#B] Renew membership in supervillain club :universe:ambition::
+  test:       In  10 d.:   ambition    TODO [#A] Take over the universe :universe:ambition:
+  test:       In  16 d.:   bills       TODO [#B] Internet                :bills:
+  test:       In  27 d.:   spaceship   TODO [#A] Spaceship lease :bills:spaceship:
+  test:       In  53 d.:   travel      WAITING Visit the moon :universe:ambition::space:travel:
+  test:       In  77 d.:   planet      TODO Visit Mars :universe:ambition::space:travel:planet:
 "))
--- test/test.el
+++ test/test.el
@@ -1,3 +1,9 @@
+;;  -*- lexical-binding: t; -*-
+
+;;; Code:
+
+;;;; Requirements
+
 (require 'org-super-agenda)
 (require 'ert)
 (require 'ht)
@@ -5,23 +11,41 @@
 
 (load-file "diary-sunrise-sunset.el")
 
+;;;; Variables
+
 (defvar org-super-agenda--test-results (ht-create))
 (defvar org-super-agenda--test-save-results nil)
-(defvar org-super-agenda--test-show-result nil)
+(defvar org-super-agenda--test-show-results nil)
+
+;;;; Commands
+
+(defun org-super-agenda--test-update-all-tests ()
+  (interactive)
+  (when (equal (read-char "Update all test results?") ?y)
+    (setq org-super-agenda--test-save-results t)
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward "(org-super-agenda--test-run")
+        (goto-char (match-beginning 0))
+        (forward-sexp)
+        (eval-last-sexp nil)))
+    (setq org-super-agenda--test-save-results nil)))
 
 (defun org-super-agenda--test-run-all ()
   (interactive)
   (ert-run-tests-interactively "^org-super-agenda--test-"))
 
-(defun org-super-agenda--test-toggle-show-result ()
+(defun org-super-agenda--test-toggle-show-results ()
   (interactive)
-  (setq org-super-agenda--test-show-result (not org-super-agenda--test-show-result))
-  (message "Showing result: %s" org-super-agenda--test-show-result))
+  (setq org-super-agenda--test-show-results (not org-super-agenda--test-show-results))
+  (message "Showing result: %s" org-super-agenda--test-show-results))
 
 (defun org-super-agenda--test-toggle-save-results ()
   (interactive)
   (setq org-super-agenda--test-save-results (not org-super-agenda--test-save-results))
   (message "Saving results: %s" org-super-agenda--test-save-results))
+
+;;;; Functions
 
 (defun org-super-agenda--test-save-result (body-groups-hash result)
   (ht-set! org-super-agenda--test-results body-groups-hash result)
@@ -33,7 +57,9 @@
   (setq org-super-agenda--test-results
         (read (f-read "results.el"))))
 
-(defmacro with-org-today-date (date &rest body)
+;;;; Macros
+
+(defmacro org-super-agenda--test-with-org-today-date (date &rest body)
   "Run BODY with the `org-today' function set to return simply DATE.
   DATE should be a date-time string (both date and time must be included)."
   (declare (indent defun))
@@ -45,7 +71,12 @@
            ,@body)
        (fset 'org-today orig))))
 
-(cl-defmacro org-super-agenda--test-run (&key body let* (groups nil groups-set) (span 'day) (date "2017-07-05 00:00"))
+(cl-defmacro org-super-agenda--test-run
+    (&key (body '(org-agenda-list nil))
+          (groups nil groups-set)
+          (span 'day)
+          (date "2017-07-05 12:00")
+          let*)
   "Test BODY with GROUPS and LET* binding.
 When `org-super-agenda--test-save-results' is non-nil, save the
 result to the results file.  When
@@ -56,91 +87,126 @@ buffer and do not save the results."
      (unless org-super-agenda--test-results
        (org-super-agenda--test-load-results))
      (let ((body-groups-hash (sxhash (list ',body ,groups)))
-           result result-hash)
-       (with-org-today-date ,date
-         (let* ((org-agenda-files (list "test.org"))
-                ,(if let*
-                     `let*
-                   `(ignore nil))
-                ,(if groups-set
-                     `(org-super-agenda-groups ,groups)
-                   `(ignore nil))
-                ,(if span
-                     `(org-agenda-span ',span)
-                   `(ignore nil))
-                string)
-           ,body
-           ;; We ignore the text properties.  This should be the right
-           ;; thing to do, since we never modify them.  It also makes
-           ;; the results actually legible.
-           (setq result (buffer-substring-no-properties 1 (point-max)))
-           (unless org-super-agenda--test-show-result
-             (kill-buffer))))
-       (when (and org-super-agenda--test-save-results
-                  (not org-super-agenda--test-show-result))
-         ;; Save test result
-         (org-super-agenda--test-save-result body-groups-hash result))
-       (unless org-super-agenda--test-show-result
-         ;; Don't give real test result when showing result buffer
-         (equal result-hash (ht-get org-super-agenda--test-results body-groups-hash))))))
+           result format-time-string-orig)
+       (unwind-protect
+           (progn
+             ;; Rebind `format-time-string' so it always returns the
+             ;; same when no time is given, otherwise the "now" line
+             ;; in the time-grid depends on the real time when the
+             ;; test is run.  (For some reason, cl-labels/cl-flet
+             ;; don't work, so we have to use `setf' and
+             ;; `symbol-function'.  Might have something to do with
+             ;; lexical-binding.)
+             (setf (symbol-function 'format-time-string-orig)
+                   (symbol-function 'format-time-string))
+             (setf (symbol-function 'format-time-string)
+                   (lambda (format-string &optional time zone)
+                     (if time
+                         (format-time-string-orig format-string time zone)
+                       (concat (second (s-split " " ,date)) " "))))
+             (org-super-agenda--test-with-org-today-date ,date
+               (let* ((org-agenda-files (list "test.org"))
+                      ,(if let*
+                           `let*
+                         `(ignore nil))
+                      ,(if groups-set
+                           `(org-super-agenda-groups ,groups)
+                         `(ignore nil))
+                      ,(if span
+                           `(org-agenda-span ',span)
+                         `(ignore nil))
+                      string)
+                 ,body
+                 ;; We ignore the text properties.  This should be the right
+                 ;; thing to do, since we never modify them.  It also makes
+                 ;; the results actually legible.
+                 (setq result (buffer-substring-no-properties 1 (point-max)))
+                 (unless org-super-agenda--test-show-results
+                   (kill-buffer))))
+             (when (and org-super-agenda--test-save-results
+                        (not org-super-agenda--test-show-results))
+               ;; Save test result
+               (org-super-agenda--test-save-result body-groups-hash result))
+             (unless org-super-agenda--test-show-results
+               ;; Don't give real test result when showing result buffer
+               (equal result (ht-get org-super-agenda--test-results body-groups-hash))))
+         ;; Reset current-time function
+         (setf (symbol-function 'format-time-string) (symbol-function 'format-time-string-orig))))))
 
 ;;;; Tests
 
 (ert-deftest org-super-agenda--test-no-groups ()
-  (org-super-agenda--test-run
-   :body (org-agenda-list nil)
-   :groups nil))
+  (should (org-super-agenda--test-run
+           :groups nil)))
+
+(ert-deftest org-super-agenda--test-time-grid-with-unprioritized-order-100 ()
+  (should (org-super-agenda--test-run
+           :groups '((:name "Today"
+                            :time-grid t)
+                     (:name "Unprioritized"
+                            :not (:priority>= "C")
+                            :order 100)))))
 
 (ert-deftest org-super-agenda--test-priority>=B ()
-  (org-super-agenda--test-run
-   :body (org-agenda-list nil)
-   :groups '((:priority>= "B"))))
+  (should (org-super-agenda--test-run
+           :groups '((:priority>= "B")))))
+
+(ert-deftest org-super-agenda--test-priority<B-with-order-100 ()
+  (should (org-super-agenda--test-run
+           :groups '((:priority< "B" :order 100)))))
+
+(ert-deftest org-super-agenda--test-heading-regexp ()
+  (should (org-super-agenda--test-run
+           :groups '((:heading-regexp "moon")))))
 
 (ert-deftest org-super-agenda--test-main-example ()
-  (org-super-agenda--test-run
-   :body (org-agenda nil "a")
-   :groups '(;; Each group has an implicit boolean OR operator between its selectors.
-             (:name "Today" ; Optionally specify section name
-                    :time-grid t ; Items that have a time associated
-                    :todo "TODAY") ; Items that have this TODO keyword
-             (:name "Important"
-                    ;; Single arguments given alone
-                    :tag "bills"
-                    :priority "A")
-             ;; Set order of multiple groups at once
-             (:order-multi (2 (:name "Shopping in town"
-                                     ;; Boolean AND group matches items that match all subgroups
-                                     :and (:tag "shopping" :tag "@town"))
-                              (:name "Food-related"
-                                     ;; Multiple args given in list with implicit OR
-                                     :tag ("food" "dinner"))
-                              (:name "Personal"
-                                     :habit t
-                                     :tag "personal")
-                              (:name "Space-related (non-moon-or-planet-related)"
-                                     ;; Regexps match case-insensitively on the entire entry
-                                     :and (:regexp ("space" "NASA")
-                                                   ;; Boolean NOT also has implicit OR between selectors
-                                                   :not (:regexp "moon" :tag "planet")))))
-             ;; Groups supply their own section names when none are given
-             (:todo "WAITING" :order 8) ; Set order of this section
-             (:todo ("SOMEDAY" "TO-READ" "CHECK" "TO-WATCH" "WATCHING")
-                    ;; Show this group at the end of the agenda (since it has the
-                    ;; highest number). If you specified this group last, items
-                    ;; with these todo keywords that e.g. have priority A would be
-                    ;; displayed in that group instead, because items are grouped
-                    ;; out in the order the groups are listed.
-                    :order 9)
-             (:priority<= "B"
-                          ;; Show this section after "Today" and "Important", because
-                          ;; their order is unspecified, defaulting to 0. Sections
-                          ;; are displayed lowest-number-first.
-                          :order 1)
-             ;; After the last group, the agenda will display items that didn't
-             ;; match any of these groups, with the default order position of 99
-             )))
+  (should (org-super-agenda--test-run
+           :groups '(;; Each group has an implicit boolean OR operator between its selectors.
+                     (:name "Today" ; Optionally specify section name
+                            :time-grid t ; Items that have a time associated
+                            :todo "TODAY") ; Items that have this TODO keyword
+                     (:name "Important"
+                            ;; Single arguments given alone
+                            :tag "bills"
+                            :priority "A")
+                     ;; Set order of multiple groups at once
+                     (:order-multi (2 (:name "Shopping in town"
+                                             ;; Boolean AND group matches items that match all subgroups
+                                             :and (:tag "shopping" :tag "@town"))
+                                      (:name "Food-related"
+                                             ;; Multiple args given in list with implicit OR
+                                             :tag ("food" "dinner"))
+                                      (:name "Personal"
+                                             :habit t
+                                             :tag "personal")
+                                      (:name "Space-related (non-moon-or-planet-related)"
+                                             ;; Regexps match case-insensitively on the entire entry
+                                             :and (:regexp ("space" "NASA")
+                                                           ;; Boolean NOT also has implicit OR between selectors
+                                                           :not (:regexp "moon" :tag "planet")))))
+                     ;; Groups supply their own section names when none are given
+                     (:todo "WAITING" :order 8) ; Set order of this section
+                     (:todo ("SOMEDAY" "TO-READ" "CHECK" "TO-WATCH" "WATCHING")
+                            ;; Show this group at the end of the agenda (since it has the
+                            ;; highest number). If you specified this group last, items
+                            ;; with these todo keywords that e.g. have priority A would be
+                            ;; displayed in that group instead, because items are grouped
+                            ;; out in the order the groups are listed.
+                            :order 9)
+                     (:priority<= "B"
+                                  ;; Show this section after "Today" and "Important", because
+                                  ;; their order is unspecified, defaulting to 0. Sections
+                                  ;; are displayed lowest-number-first.
+                                  :order 1)
+                     ;; After the last group, the agenda will display items that didn't
+                     ;; match any of these groups, with the default order position of 99
+                     ))))
 
 (ert-deftest org-super-agenda--test-auto-groups ()
-  (org-super-agenda--test-run
-   :body (org-agenda-list nil nil 'day)
-   :groups '((:auto-groups t))))
+  (should (org-super-agenda--test-run
+           :groups '((:auto-groups t)))))
+
+(ert-deftest org-super-agenda--test-discard-with-2-regexps ()
+  (should (org-super-agenda--test-run
+           :groups '((:discard (:regexp "pizza"
+                                        :regexp "groceries"))))))
