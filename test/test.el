931af5e00b2fa417b0673c2cbaa8ca5a664eafc6
Adam Porter <adam@alphapapa.net>
2020-01-02 01:46:02 -0600

    Tests: Move diary-sunset code into main file
    Not sure what the point was of having it separate.  It just makes
    loading the test file error-prone.

--- test/test.el
+++ test/test.el
@@ -4,7 +4,9 @@
 
 ;;;; Requirements
 
+;; FIXME: Shouldn't be using `cl'.
 (require 'cl)
+(require 'cl-lib)
 (require 'cus-edit)
 (require 'org-habit)
 (require 'org-super-agenda)
@@ -15,7 +17,39 @@
 (require 'f)
 (require 's)
 
-(load-file "diary-sunrise-sunset.el")
+;;;; Diary-sunset
+
+(require 'solar)
+(require 'cal-dst)
+
+(defun org-super-agenda--test-diary-sunrise-sunset-split ()
+  "Split `diary-sunrise-sunset' into sunrise, sunset, and daylight hours."
+  (let* ((calendar-latitude 0)
+         (calendar-longitude 0)
+         (calendar-time-zone (if (dst-in-effect (date-to-day org-super-agenda--test-date))
+                                 0
+                               60))
+         (string (diary-sunrise-sunset))
+         (regexp (rx (group "Sunrise " (1+ (or digit ":")) (or "am" "pm")) " "
+                     (group "(" (1+ alpha) ")") ", "
+                     (group "sunset " (1+ (or digit ":")) (or "am" "pm")) " "
+                     (group "(" (1+ alpha) ")")
+                     (1+ anything)
+                     "(" (group (1+ (or digit ":")))
+                     ))
+         (sunrise (progn
+                    (string-match regexp string)
+                    (match-string 1 string)) )
+         (sunset (capitalize (match-string 3 string)))
+         (daylight (format "%s of daylight" (match-string 5 string))))
+    (list sunrise sunset daylight)))
+
+(defun org-super-agenda--test-diary-sunrise ()
+  (let ((s (org-super-agenda--test-diary-sunrise-sunset-split)))
+    (format "%s (%s)" (first s) (third s))))
+
+(defun org-super-agenda--test-diary-sunset ()
+  (cl-second (org-super-agenda--test-diary-sunrise-sunset-split)))
 
 ;;;; Variables
 
