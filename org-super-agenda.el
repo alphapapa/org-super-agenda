4ab157c04d2af60d39da3082918ebb6a05bbe4c8
Jeff Zohrab <jzohrab@gmail.com>
2020-03-11 15:07:15 -0600

    Update obsolete function call

--- org-super-agenda.el
+++ org-super-agenda.el
@@ -514,9 +514,9 @@ COMPARATOR is a grouping comparison operator."
      ,(concat docstring "\nArgument is a time-duration string, like \"5\" or \"0:05\" for 5 minutes.")
      :section-name (concat "Effort " ,(symbol-name name) " "
                            (s-join " or " args) " items")
-     :let* ((effort-minutes (org-duration-string-to-minutes (car args))))
+     :let* ((effort-minutes (org-duration-to-minutes (car args))))
      :test (when-let ((item-effort (org-find-text-property-in-string 'effort item)))
-             (,comparator (org-duration-string-to-minutes item-effort) effort-minutes))))
+             (,comparator (org-duration-to-minutes item-effort) effort-minutes))))
 
 (org-super-agenda--defeffort-group <
   "Group items that are less than (or equal to) the given effort."
