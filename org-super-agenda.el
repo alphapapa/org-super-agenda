ee3379ae92b90c084717fb2a7614060ce12283cb
Adam Porter <adam@alphapapa.net>
2023-09-26 14:18:57 -0500

    Change/Fix: (parent) Use org-entry-get

--- org-super-agenda.el
+++ org-super-agenda.el
@@ -1097,7 +1097,7 @@ key and as the header for its group."
 (org-super-agenda--def-auto-group parent "their parent heading"
   :key-form (org-super-agenda--when-with-marker-buffer (org-super-agenda--get-marker item)
               (when (org-up-heading-safe)
-                (org-get-heading 'notags 'notodo))))
+                (org-entry-get nil "ITEM"))))
 
 ;;;;; Dispatchers
 
